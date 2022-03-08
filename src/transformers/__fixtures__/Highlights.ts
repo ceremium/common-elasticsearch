3919ade547d42ddbc1c6ae40dc39d930d6af253f
Yassire Elhani <yassire@makeandship.com>
2022-03-08 15:42:16 +0000

    support highlights

---
+++ src/transformers/__fixtures__/Highlights.ts
@@ -0,0 +1,77 @@
+export default {
+  valid: {
+    _index: 'tte-service-live',
+    _id: '115',
+    _score: null,
+    _source: {
+      title:
+        'Healthcare AI innovator Prognos finds frequent doses of new insight can benefit its customers',
+      description:
+        'We delivered a new automated data experience to over 15 pharma brands, helping them provide better and faster treatment options to patients.',
+      image:
+        '/content/dam/pwc/us/en/tech-effect/content/images/cta/casestudy-prognos.jpg',
+      published: '2018-11-29',
+      isPage: true,
+      isVideo: false,
+      url: 'https://www.pwc.com/us/en/tech-effect/ghosts/ai-analytics/prognos-ai-healthcare.html',
+      isGhost: true,
+      tags: [
+        'pwc-us:content-type/case-study',
+        'pwc-us:tech-effect/audience/tech-led',
+        'pwc-us:tech-effect/templates/feature',
+        'pwc-us:tech-effect/topics/ai-and-analytics',
+        'pwc-us:tech-effect',
+        'pwc-us:tech-effect/audience/business-led',
+      ],
+      type: 'case-study',
+      readingTime: null,
+      template: 'feature',
+      lastUpdated: '2022-03-08T13:04:31.229Z',
+      engagement: null,
+      contentType: ['case-study'],
+      audience: ['tech-led', 'business-led'],
+      topics: ['ai-and-analytics'],
+    },
+    highlight: {
+      'title.phrased': [
+        'Healthcare <strong>AI</strong> innovator Prognos finds frequent doses of new insight can benefit its customers',
+      ],
+    },
+    sort: [1543449600000],
+  },
+  empty: {
+    _index: 'tte-service-live',
+    _id: '115',
+    _score: null,
+    _source: {
+      title:
+        'Healthcare AI innovator Prognos finds frequent doses of new insight can benefit its customers',
+      description:
+        'We delivered a new automated data experience to over 15 pharma brands, helping them provide better and faster treatment options to patients.',
+      image:
+        '/content/dam/pwc/us/en/tech-effect/content/images/cta/casestudy-prognos.jpg',
+      published: '2018-11-29',
+      isPage: true,
+      isVideo: false,
+      url: 'https://www.pwc.com/us/en/tech-effect/ghosts/ai-analytics/prognos-ai-healthcare.html',
+      isGhost: true,
+      tags: [
+        'pwc-us:content-type/case-study',
+        'pwc-us:tech-effect/audience/tech-led',
+        'pwc-us:tech-effect/templates/feature',
+        'pwc-us:tech-effect/topics/ai-and-analytics',
+        'pwc-us:tech-effect',
+        'pwc-us:tech-effect/audience/business-led',
+      ],
+      type: 'case-study',
+      readingTime: null,
+      template: 'feature',
+      lastUpdated: '2022-03-08T13:04:31.229Z',
+      engagement: null,
+      contentType: ['case-study'],
+      audience: ['tech-led', 'business-led'],
+      topics: ['ai-and-analytics'],
+    },
+    sort: [1543449600000],
+  },
+};
